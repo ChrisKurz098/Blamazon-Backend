4b252b2654f4421f9b6046adab5710f5f1308185
CKURZ <ckurz098@gmail.com>
2022-02-07 10:48:39 -0800

    associations made

--- models/index.js
+++ models/index.js
@@ -4,14 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'catagory_name'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag_link',
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  as: 'tag_link',
+  foreignKey: 'tag_id'
+})
 module.exports = {
   Product,
   Category,
